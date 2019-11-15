ddf4bd284ed3d2d921c5b763360ebea94f02cf20
Mark Yan <myan@sierrawireless.com>
2019-11-15 15:59:56 +0800

    Reverse TMC serial port enable signal

--- Marlin/src/pins/pins_MKS_SBASE.h
+++ Marlin/src/pins/pins_MKS_SBASE.h
@@ -81,9 +81,9 @@
 #define E0_DIR_PIN         P0_22
 #define E0_ENABLE_PIN      P0_21
 
-#define E1_STEP_PIN        P2_08
-#define E1_DIR_PIN         P2_13
-#define E1_ENABLE_PIN      P4_29
+//#define E1_STEP_PIN        P2_08
+//#define E1_DIR_PIN         P2_13
+//#define E1_ENABLE_PIN      P4_29
 
 //
 // Temperature Sensors
